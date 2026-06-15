7e13d7c56741ceb649e73727369323b2e0d9dae4
Stefanus <stefanusdwichristianto@gmail.com>
2026-06-15 08:42:50 +0700

    init: Logic To Pixels.

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://stefanusdwichristianto.my.id/sitemap.xml',
+  }
+}
