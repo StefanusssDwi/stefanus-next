28fcc0a0cc82abb6fe08eaefc1c218539c0d9e4d
Stefanus <stefanusdwichristianto@gmail.com>
2026-06-15 08:35:47 +0700

    init: Logic To Pixels.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,7 +6,35 @@ import SplashWrapper from "../components/SplashWraper";
 
 export const metadata = {
   title: "STEFANUS | Fullstack Developer",
-  description: "Stefanus' professional portfolio, a full-stack developer. Combines solid IT competency with precision front-end design to build impactful modern web applications.",
+  description: "Portofolio profesional Stefanus, seorang Fresh Graduate Fullstack Developer. Logic to Pixels.",
+  
+  keywords: ["Stefanus", "Frontend Developer", "Fullstack Developer", "Portofolio IT", "ReactJS", "NextJS", "Fresh Graduate"],
+  
+  authors: [{ name: "Stefanus Dwi Christianto" }],
+
+  openGraph: {
+    title: "STEFANUS | Fullstack Developer",
+    description: "Merakit aplikasi web modern dari hulu ke hilir.",
+    url: "",
+    siteName: "Portofolio Stefanus",
+    images: [
+      {
+        url: "https://stefanusdwichristianto.my.id/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Preview Portofolio Stefanus",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "STEFANUS | Fullstack Developer",
+    description: "Merakit aplikasi web modern dari hulu ke hilir.",
+    images: ["https://stefanusdwichristianto.my.id/og-image.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
